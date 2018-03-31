fa3126938cf3cefbea0e51a8d2d90df5cb885bd1
sachinkj <sachinkj@in.ibm.com>
2018-03-31 13:16:42 +0530

    updated pipeline.sh again

--- .bluemix/pipeline.sh
+++ .bluemix/pipeline.sh
@@ -374,7 +374,7 @@ printf "\n ----- created ca card ----- \n"
 date
 printf "\n ---- Create admin card ----- \n "
 composer card create -f adminCard.card -p ./config/connection-profile.json -u admin -c ./credentials/admin-pub.pem -k ./credentials/admin-priv.pem --role PeerAdmin --role ChannelAdmin
-
+composer card delete -n admin-org2@blockchain-network
 composer card import -f adminCard.card -n admin-org2@blockchain-network
 date
 printf "\n ---- Created admin card ----- \n "
